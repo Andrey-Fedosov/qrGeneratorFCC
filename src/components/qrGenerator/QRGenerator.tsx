733c988709f27622b0933bf43449502642b97223
Andrey-Fedosov <andrefedosov@gmail.com>
2024-02-26 23:11:10 +0300

    feature: add generate qr code logic

--- src/components/qrGenerator/QRGenerator.tsx
+++ src/components/qrGenerator/QRGenerator.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import QRCode from "react-qr-code";
 
 export function QRGenerator() {
-  return <div>This is QR Generator</div>;
+  const [qrCode, setQrCode] = useState("");
+  const [input, setInput] = useState("");
+
+  const generateHandler = () => {
+    setQrCode(input);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <h2>This is QR Generator</h2>
+      <div>
+        <input
+          type="text"
+          name="qr-code"
+          value={input}
+          placeholder="enter your value here"
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setInput(e.currentTarget.value)
+          }
+        />
+        <button
+          disabled={input && input.trim() !== "" ? false : true}
+          onClick={generateHandler}
+        >
+          Generate
+        </button>
+      </div>
+      <QRCode id="qr-code-value" value={qrCode} />
+    </div>
+  );
 }
